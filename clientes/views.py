9a28b849b74d50f2a93016099e16b56648b5b52c
JesusValmiki <Jesusvalmiki@mail.com>
2023-05-15 00:57:01 -0600

    importe los modelos a clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cliente
 # Create your views here.
 # def index(request):
 #     return render(request,"index.html")
 
 def clientes(request):
-    return render(request,"clientes.html")
+    clientes = Cliente.objects.all()
+    return render(request,"clientes.html", {"clientes": clientes})
 

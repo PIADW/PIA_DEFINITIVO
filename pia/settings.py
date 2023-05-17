08b94e3754fcb1585d611183950c1fed8e84d3e8
Samuel Obed Garcia Gomez <samuel.gg02@gmail.com>
2023-05-16 19:11:49 -0600

    3 paginas de 5 terminadas

--- pia/settings.py
+++ pia/settings.py
@@ -60,7 +60,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'pia/templates')
-            ],
+            ,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'pia/static')
